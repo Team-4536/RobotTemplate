3b1247a48ef2916a0a90e207af4bbf81bccd0913
Kevin Tran <26168327+Kinzerfest@users.noreply.github.com>
2019-06-11 11:43:20 -0500

    2019 Control System

---
+++ src/main/java/org/minutebots/frc2020/Robot.java
@@ -0,0 +1,38 @@
+package org.minutebots.frc2020;
+
+import edu.wpi.first.wpilibj.TimedRobot;
+
+public class Robot extends TimedRobot {
+  /**
+   * This function is run when the robot is first started up and should be used
+   * for any initialization code.
+   */
+  @Override
+  public void robotInit() {
+  }
+
+  @Override
+  public void autonomousInit() {
+  }
+
+  @Override
+  public void autonomousPeriodic() {
+  }
+
+  @Override
+  public void teleopInit() {
+  }
+
+  @Override
+  public void teleopPeriodic() {
+  }
+
+  @Override
+  public void testInit() {
+  }
+
+  @Override
+  public void testPeriodic() {
+  }
+
+}
